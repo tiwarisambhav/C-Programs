467520f9af4fe5857c10e8ea76610c6cb35d8698
tiwarisambhav <125248752+tiwarisambhav@users.noreply.github.com>
2023-02-19 23:04:17 +0530

    main

---
+++ Conditional Operator/evenodd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+               int a;
+               printf("Enter the no");
+               scanf("%d",&a);
+               if(a%2==0){
+                              printf("The number is even");
+
+               }else{
+                              printf("The number is odd");
+               }
+               return 0;
+}
