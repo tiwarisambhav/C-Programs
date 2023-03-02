7995ba1fc6ad19156b0a2f3d3a7ea54836abccc9
tiwarisambhav <125248752+tiwarisambhav@users.noreply.github.com>
2023-03-02 22:25:00 +0530

    main

---
+++ If else/divisible.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+               int  a;
+               scanf("%d",&a); 
+               if(a%5==0&&a%11==0){
+                              printf("a is divisible by 5 and 11");
+
+               }else{
+                              printf("a is not divisible");
+               }
+}
