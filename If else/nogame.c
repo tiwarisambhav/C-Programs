196a33185175e14ec0cf381b8d5b98965044e695
tiwarisambhav <125248752+tiwarisambhav@users.noreply.github.com>
2023-03-02 20:43:23 +0530

    main

---
+++ If else/nogame.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+               int a;
+               scanf("%d",&a);
+               if(a>0){
+                              printf("a is positive");
+               }if(a<0){
+                              printf("a is negative");
+               }else{
+                              printf("a is zero");
+               }
+}
