5fdec7cb82e0719917d93fb53eb6f481f25c99dc
tiwarisambhav <125248752+tiwarisambhav@users.noreply.github.com>
2023-03-04 13:28:51 +0530

    main

---
+++ If else/specialch.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+               char ch;
+               scanf("%c",&ch);
+               if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z'){
+                              if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+                                             printf("VOWEL");
+                                             }else {
+                                                            printf("Consonant");
+                                             }
+
+               }else if (ch>='0'&&ch<='9'){
+                              printf("Digit");
+               }else{
+                              printf("Special Character");
+               }
+}
--- If else/vowaph.c
+++ If else/vowaph.c
@@ -1,4 +1,14 @@
 #include<stdio.h>
 int main(){
-               
+               char ch;
+               scanf("%c",&ch);
+               if(ch>=65&&ch<=91||ch>=97&&ch<=123){
+                              if(ch=='a'||ch=='e'||ch=='o'||ch=='i'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+                                             printf("ch is vowel");
+                              }else{
+                                             printf("ch is consonant");
+                              }
+               }
+               return 0;
+
 }
