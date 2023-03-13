971f2ecf6e5d89d23f38fb9f40ead7e2dcd59c0e
tiwarisambhav <125248752+tiwarisambhav@users.noreply.github.com>
2023-03-13 10:41:04 +0530

    main

---
+++ Days.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+               int a;
+               scanf("%d",&a);
+               switch(a)
+               {
+               
+                              case 1:
+                              printf("Monday");
+                              break;
+                              case 2:
+                              printf("Tuesday");
+                              break;
+                              case 3:
+                              printf("Wednesday");
+                              break;
+                              case 4:
+                              printf("Tuesday");
+                              break;
+                              case 5:
+                              printf("Friday");
+                              break;
+                              case 6:
+                              printf("Saturaday");
+                              break;
+                              case 7 :
+                              printf("Sunday");
+                              break;
+                              default:
+                              printf("Invalid no");
+
+
+               }
+               return 0;
+}
